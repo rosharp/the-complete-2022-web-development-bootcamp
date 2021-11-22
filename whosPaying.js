3a5e2eadd3ff8300ad72a75732d5372083e643c5
Ро <ro.sharp@MacBook-Pro-Ro.local>
2021-11-22 15:04:13 +1000

    Who Is Paying?

---
+++ whosPaying.js
@@ -0,0 +1,15 @@
+function whosPaying(names) {
+
+/******Don't change the code above*******/
+
+    //Write your code here.
+    var listOfNames = ["Angela", "Ben", "Jenny", "Michael", "Chloe"];
+    var randomPersonPosition = Math.floor(Math.random() * listOfNames.length)
+    var randomPerson = listOfNames[randomPersonPosition];	 
+    return randomPerson + " is going to buy lunch today!";
+
+
+
+
+/******Don't change the code below*******/
+}
