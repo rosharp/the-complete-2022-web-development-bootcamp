96d229080bb3efa731c885a7499324d59a8176b5
Ро <ro.sharp@MacBook-Pro-Ro.local>
2021-11-22 15:11:03 +1000

    Initial commit

--- guestList.js
+++
@@ -1,8 +0,0 @@
-var guestList = ["Angela", "Jack", "Fas", "Jesus", "Lara", "Jason"];
-
-var guestName = prompt("Enter your name:");
-
-if guestList.includes(guestName) {
-	alert("Welcome, " + guestName + "!");
-}
-
