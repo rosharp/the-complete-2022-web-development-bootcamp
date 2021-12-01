95fdf1189c282c4f953fa30f439040ecd6adc8ea
Ро <ro.sharp@MacBook-Pro-Ro.local>
2021-12-01 14:26:58 +1000

    Drum Kit in process

---
+++ Intermediate JavaScript/Drum Kit/index.js
@@ -0,0 +1,10 @@
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i<numberOfDrumButtons; i++) {
+
+document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+	var audio = new Audio('sounds/tom-1.mp3');
+        audio.play();
+	}); 
+}
+
