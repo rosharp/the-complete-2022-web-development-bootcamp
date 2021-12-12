43c3f52488f325f7829b6be3394f9415dccc8f20
Ро <ro.sharp@MacBook-Pro-Ro.local>
2021-12-12 20:12:02 +1000

    Added Node folder

---
+++ Introduction to Node.js/Intro/index.js
@@ -0,0 +1,11 @@
+//jshint esversion:6
+
+const supervillains = require('supervillains');
+ 
+supervillains.all;
+//=> ['Abattoir', 'Able Crown', …]
+ 
+//=> 'Mud Pack'
+
+console.log(supervillains.random());
+
