c94bcc1d5c4ff0e275433552491d95622996cb91
rosharp <ro.moreko@gmail.com>
2022-01-24 21:52:46 +1000

    Commit in progress

---
+++ RESTful API/Wiki-API/app.js
@@ -0,0 +1,87 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const ejs = require("ejs");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/WikiDB", {
+  useNewUrlParser: true
+});
+
+// mongodb+srv://admin-ro:ahugun45@cluster0.phzww.mongodb.net/WikiDB?retryWrites=true&w=majority
+
+const articleSchema = {
+  title: String,
+  content: String,
+};
+
+const Article = mongoose.model("Article", articleSchema);
+
+/////////////////////////// For Multiple Requests ///////////////////////////
+
+app.route("/articles")
+
+  .get(function(req, res) {
+    Article.find({}, function(err, foundArticles) {
+      if (!err) {
+        res.send(foundArticles);
+      } else {
+        res.send(err);
+      }
+
+    });
+  })
+
+  .post(function(req, res) {
+    const newArticle = new Article({
+      title: req.body.title,
+      content: req.body.content,
+    });
+
+    newArticle.save(function(err) {
+      if (!err) {
+        res.send("Successfully added a new article.");
+      } else {
+        res.send(err);
+      };
+    });
+  })
+
+  .delete(function(req, res) {
+    Article.deleteMany(function(err) {
+      if (!err) {
+        res.send("Successfully deleted.");
+      } else {
+        console.log(err);
+      }
+    });
+  });
+
+/////////////////////////// For Single Requests ///////////////////////////
+
+app.route("/articles/:articleTitle")
+
+  .get(function(req, res) {
+    Article.findOne({title: req.params.articleTitle}, function(err, foundArticle) {
+      if (foundArticle) {
+        res.send(foundArticle);
+      } else {
+        res.send("No articles matching this title was found.");
+      }
+    });
+  });
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000.");
+});
