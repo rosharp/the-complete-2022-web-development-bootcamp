97fed3d6776f7d92636d5fe47c8e166d90cbc698
rosharp <ro.moreko@gmail.com>
2022-02-07 18:18:28 +1000

    useState lesson added

---
+++ React/usestate-hook-forked/src/components/App.jsx
@@ -0,0 +1,26 @@
+import React, {useState} from "react";
+import ReactDOM from "react";
+
+
+
+function App() {
+  const [count, setCount] = useState(0);
+ 
+  function increase() {
+    setCount(count + 1);
+  }
+
+  function decrease() {
+    setCount(count - 1);
+  }
+  
+  return (
+    <div className="container">
+    <h1>{count}</h1>
+    <button onClick={increase}>+</button>
+    <button onClick={decrease}>-</button>
+    </div>
+  )
+}
+
+export default App;
