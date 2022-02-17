d748bcd17514383ce1b3c59afafa5b53d731b761
Ро <ro.sharp@MacBook-Pro-Ro.local>
2022-02-17 14:31:33 +1000

    React in progress

---
+++ React/managing-a-component-tree/src/components/TodoItem.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function TodoItem(props) {
+  return (
+    <div
+      onClick={() => {
+        props.onChecked(props.id);
+      }}
+    >
+      <li>{props.text}</li>
+    </div>
+  );
+}
+
+export default TodoItem;
